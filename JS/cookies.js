3eacc62d65c90e2058869c05afe6dad1c814cf5f
Owen Cocjin <owen_jamescf@hotmail.com>
2021-08-31 01:27:51 -0400

    Added statistics monitor

--- JS/cookies.js
+++ JS/cookies.js
@@ -1,2 +1,6 @@
-//Set cookies so user doesn't need to wait for the title page every time.
-document.cookie
+//Add statistics monitor
+var pxl=document.createElement("img");
+pxl.src="http://127.0.0.1:7431"+window.location.pathname;
+pxl.width=0;
+pxl.height=0;
+document.getElementsByTagName("footer")[0].appendChild(pxl);
