d521a84547ecf5e683e78ed4f19e61052939fa43
Owen Cocjin <owen_jamescf@hotmail.com>
2021-08-31 01:41:00 -0400

    Added statistics monitoring

--- JS/cookies.js
+++ JS/cookies.js
@@ -3,4 +3,6 @@ var pxl=document.createElement("img");
 pxl.src="http://155.138.133.28:7431"+window.location.pathname;
 pxl.width=0;
 pxl.height=0;
+pxl.style.position="fixed";
+pxl.style.opacity=0;
 document.getElementsByTagName("footer")[0].appendChild(pxl);
