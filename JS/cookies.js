c6769954bf15a4e5b0bb068b56dc0b0caed31f98
Owen Cocjin <owen_jamescf@hotmail.com>
2021-08-31 01:30:59 -0400

    Updated path

--- JS/cookies.js
+++ JS/cookies.js
@@ -1,6 +1,6 @@
 //Add statistics monitor
 var pxl=document.createElement("img");
-pxl.src="http://127.0.0.1:7431"+window.location.pathname;
+pxl.src="http://155.138.133.28:7431"+window.location.pathname;
 pxl.width=0;
 pxl.height=0;
 document.getElementsByTagName("footer")[0].appendChild(pxl);
