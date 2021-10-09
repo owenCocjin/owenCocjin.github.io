cfd805f21fa0ed03791e821491718a161753b271
Owen Cocjin <owen_jamescf@hotmail.com>
2021-10-08 23:43:28 -0400

    Migrated site

--- JS/cookies.js
+++
@@ -1,8 +0,0 @@
-//Add statistics monitor
-var pxl=document.createElement("img");
-pxl.src="https://dsrv001.ddns.net:7431"+window.location.pathname;
-pxl.width=0;
-pxl.height=0;
-pxl.style.position="fixed";
-pxl.style.opacity=0;
-document.getElementsByTagName("footer")[0].appendChild(pxl);
